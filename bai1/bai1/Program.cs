a23846e5063837462c3909ca143f361df8c018ab
Your Name <you@example.com>
2023-08-28 19:33:45 +0700

    first commit

---
+++ bai1/bai1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToaDo td2 = new ToaDo(2, 3, "A");
+            Console.WriteLine(td2.toString());
+            td2.setx(5);
+            td2.sety(7);
+            td2.setten("B");
+            Console.WriteLine(td2.toString());
+            Console.ReadLine();
+        }
+    }
+}
